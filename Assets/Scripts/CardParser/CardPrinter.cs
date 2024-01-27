b362e04f01113a52197aa3d5165f41291c30bd25
bitowl <git@bitowl.de>
2024-01-27 13:02:51 +0100

    Replace <aan> depending on if the next word is a noun or an adjective

--- Assets/Scripts/CardParser/CardPrinter.cs
+++ Assets/Scripts/CardParser/CardPrinter.cs
@@ -22,10 +22,47 @@ public class CardPrinter
 
     private static string ReplaceNounPlaceholders(string text, CardParser.Noun noun)
     {
+        // a / an handling
+        while (text.IndexOf("<aan>") >= 0)
+        {
+            var aanIndex = text.IndexOf("<aan>");
+            var before = text.Substring(0, aanIndex);
+            var after = text.Substring(aanIndex + 5);
+
+            var nextNoun = after.IndexOf("<noun>");
+            var nextAdjective = after.IndexOf("<adjective>");
+
+            if (nextNoun == 1)
+            {
+                if (noun.indefiniteArticle == null)
+                {
+                    Debug.LogError($"Noun {noun.name} does not have aan defined!");
+                    noun.indefiniteArticle = "a";
+                }
+                text = before + noun.indefiniteArticle + after;
+            }
+            else if (nextAdjective == 1)
+            {
+                if (noun.indefiniteArticleAdjective == null)
+                {
+                    Debug.LogError($"The adjective {noun.adjective} of noun {noun.name} does not have aan defined!");
+                    noun.indefiniteArticleAdjective = "a";
+                }
+                text = before + noun.indefiniteArticleAdjective + after;
+            }
+            else
+            {
+                Debug.LogError("After <aan> there is something else than a <noun> or <adjective>.");
+                Debug.Log($"Next Noun: {nextNoun}");
+                Debug.Log($"Next Adjective: {nextAdjective}");
+                text = before + "<error>" + after;
+            }
+        }
+
         text = text.Replace("<noun>", noun.name);
-        text = text.Replace("<aan>", noun.indefiniteArticle);
         text = text.Replace("<pronoun>", pronouns[noun.gender]);
         text = text.Replace("<adjective>", noun.adjective);
+
         return text;
     }
 }
