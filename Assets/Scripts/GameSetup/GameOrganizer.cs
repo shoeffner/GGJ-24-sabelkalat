be672c7e7cd0f7f1801cbec7a644f44c01023d80
lauxmant <tobias_holger.lauxmann@student.reutlingen-university.de>
2024-01-27 13:32:04 +0100

    score in game oranizer

--- Assets/Scripts/GameSetup/GameOrganizer.cs
+++ Assets/Scripts/GameSetup/GameOrganizer.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class GameOrganizer : Singleton<GameOrganizer>
 {
+    public UnityAction OnGameOver;
+
     [SerializeField] private GameSetup gameSetup;
     [SerializeField] private Audience audience;
+    private int currentScore = 0;
 
     private int currentRoundIndex = 0;
 
@@ -12,12 +16,19 @@ public class GameOrganizer : Singleton<GameOrganizer>
         audience.SetNewViewers(gameSetup.gameRounds[currentRoundIndex].viewers);
     }
 
+    public void AddScore(int score)
+    {
+        currentScore = Mathf.Clamp(currentScore + score, gameSetup.lowerScoreLimit, gameSetup.upperScoreLimit);
+    }
+
+    public int GetCurrentScore() => currentScore;
+
     public void NextRound()
     {
         currentRoundIndex++;
         if (currentRoundIndex >= gameSetup.gameRounds.Count)
         {
-            Debug.Log("Game Over");
+            OnGameOver?.Invoke();
             return;
         }
         audience.SetNewViewers(gameSetup.gameRounds[currentRoundIndex].viewers);
